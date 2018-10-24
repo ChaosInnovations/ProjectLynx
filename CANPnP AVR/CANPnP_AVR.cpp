7fdf71e0fdd6b57d51e23fe348bf9629c85b4e44
Thomas Boland <thomas.boland@outlook.com>
2018-10-24 12:53:03 -0700

    Added CANPnP AVR Library

---
+++ CANPnP AVR/CANPnP_AVR.cpp
@@ -0,0 +1,10 @@
+/*
+ Name:		CANPnP_AVR.cpp
+ Created:	10/24/2018 8:22:35 AM
+ Author:	Thomas Boland
+ Editor:	http://www.visualmicro.com
+*/
+
+#include "CANPnP_AVR.h"
+
+
---
+++ CANPnP AVR/CANPnP_AVR.h
@@ -0,0 +1,19 @@
+/*
+ Name:		CANPnP_AVR.h
+ Created:	10/24/2018 8:22:35 AM
+ Author:	Thomas Boland
+ Editor:	http://www.visualmicro.com
+*/
+
+#ifndef _CANPnP_AVR_h
+#define _CANPnP_AVR_h
+
+#if defined(ARDUINO) && ARDUINO >= 100
+	#include "arduino.h"
+#else
+	#include "WProgram.h"
+#endif
+
+
+#endif
+
